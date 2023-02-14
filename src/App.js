8ef7117dbf9b65ba3dabe43ddf9f870dcb5e5576
Bjay02 <bolajiafolabi02@gmail.com>
2023-02-14 11:38:48 +0100

    getting started with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
+  const [role, setRole] = useState("");
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? (
         <>
-          <Employee />
-          <Employee />
-          <Employee />
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <Employee name="Bolaji" role="intern" />
+          <Employee name=" Eme" role={role} />
+          <Employee name="John" role={role} />
         </>
       ) : (
         <p>You cannot see the employees</p>
